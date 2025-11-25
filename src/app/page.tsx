6655e73b12c3fa980bccd25ed826daf028874bcb
upuptts12-arch <upuptts12@gmail.com>
2025-11-25 10:48:32 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import TopicsList from '@/components/TopicsList';
-export default function Home() {
+import React from 'react';
+
+export default function HomePage() {
   return (
     <div>
-            <h1 className="text-3xl font-bold">WebDev Topics</h1>     {' '}
-      <p className="mb-4">MongoDB CRUD Example</p>
-            <TopicsList />
-            <TopicsList />   {' '}
+      <h1 className="text-3xl font-bold">WebDev Topics </h1>
+      <p className="mb-4">MongoDB CRUD examples</p>
+      <TopicsList />
     </div>
   );
 }
